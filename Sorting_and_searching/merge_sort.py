7de2f6afc46b121d8fea3c978859985f181a162e
@rlicona <ricklicona@icloud.com>
2019-10-11 21:11:00 -0500

    Merge sort

---
+++ Sorting_and_searching/merge_sort.py
@@ -0,0 +1,60 @@
+def mergeSort(alist):
+    print("Splitting: ", alist)
+    if len(alist) > 1:
+        mid = len(alist) // 2
+        lefthalf = alist[:mid]
+        righthalf = alist[mid:]
+        print("\n\n************************")
+        print("LEFT HALF: ", lefthalf)
+        print("RIGH HALF:, ", righthalf)
+        print("************************\n\n")
+
+        print("SIGUE LEFT")
+        mergeSort(lefthalf)
+        print("SIGUE RIGHT")
+        mergeSort(righthalf)
+        print("FIN CALL")
+
+
+        i = 0
+        j = 0
+        k = 0
+
+        while i < len(lefthalf) and j < len(righthalf):
+            #print("\n Len half: ", len(lefthalf))
+            #print("\n Len right: ", len(righthalf))
+
+            if lefthalf[i] <= righthalf[j]:
+                alist[k] = lefthalf[i]
+                i = i + 1
+                print("case 1")
+                print("i: ", i)
+            else:
+                alist[k] = righthalf[j]
+                j = j + 1
+                print("case 2")
+                print("j: ", j)
+            k = k + 1
+            print("k: ", k)
+        while i < len(lefthalf):
+            alist[k] = lefthalf[i]
+            i = i + 1
+            k = k + 1
+            print("case 3")
+            print("i: ", i)
+            print("k: ", k)
+
+        while j < len(righthalf):
+            alist[k] = righthalf[j]
+            j = j + 1
+            k = k + 1
+            print("case 4")
+            print("j: ", j)
+            print("k: ", k)
+
+    print("Merging: ", alist)
+
+
+alist = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+mergeSort(alist)
+print(alist)
