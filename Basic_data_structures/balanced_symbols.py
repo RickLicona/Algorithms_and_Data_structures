a5124628be977885786400379908c835fa99bf60
@rlicona <ricklicona@icloud.com>
2019-10-21 23:12:27 -0500

    Balanced symbols

---
+++ Basic_data_structures/balanced_symbols.py
@@ -0,0 +1,42 @@
+from Basic_data_structures import stack
+
+
+def par_checker(symbol_string):
+    s = stack.Stack()
+    balanced = True
+    index = 0
+    while index < len(symbol_string) and balanced:
+        symbol = symbol_string[index]
+        if symbol in "([{":
+            s.push(symbol)
+        else:
+            if s.is_empty():
+                balanced = False
+            else:
+                top = s.pop()
+                if not matches(top, symbol):
+                    balanced = False
+        index = index + 1
+
+    if balanced and s.is_empty():
+        return True
+    else:
+        return False
+
+
+def matches(open, close):
+    print("top:", open)
+    print("symbol: ", close)
+    opens = "([{"
+    closes = ")]}"
+    print("Top index: ", opens.index(open))
+    print("Symbol index: ", closes.index(close))
+    print("**********************************")
+
+    return opens.index(open) == closes.index(close)
+
+
+print("\n\nFIRST")
+print("Is {{([][])}()} balanced? : ", par_checker('{{([][])}()}'))
+print("\n\nSECOND")
+print("Is [{()] balanced? : ", par_checker('[{()]'))
